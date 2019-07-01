e2e3b14cb383c1ea187928b4968ebfa944ffc054
Marissa Perry <ohthatmarissa@gmail.com>
2019-06-30 22:08:08 -0700

    add all the things

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList() {
+    return (
+        <Ticket
+            location="3B"
+            names="Sweetie and Booger"
+            issue="This shit is cray"/>
+    );
+}
+
+export default TicketList;
---
+++ src/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { AppContainer } from 'react-hot-loader';
+
+const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component/>
+        </AppContainer>,
+        document.getElementById('react-app-root')
+    );
+};
+
+render(App);
+
+if(module.hot) {
+    module.hot.accept('./components/App', () => {
+        render(App)
+    });
+}
+
+
+
+// function liveTime() {
+//     var heading = React.createElement('h1',  {}, 'Help Queue!');
+//     var ticketLocation = React.createElement('h3', {}, '3b');
+//     var ticketNames = React.createElement('h3', {}, 'Sweetie and Booger');
+//     var ticketIssue = React.createElement('h3', {}, "This shit is cray")
+//     var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
+
+// ReactDOM.render(
+//     app,
+//     document.getElementById('react-app-root')
+//     );
+// }
+
+// setInterval(liveTime, 1000);
